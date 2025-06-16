8ead3141f5288ae8a3e6219409c9f69a57e6aec4
Noel <noelenriqueascanio@gmail.com>
2025-06-16 14:08:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,4 +12,4 @@ routes(app)
 app.config['SECRET_KEY'] = "1234"
 
 # Recuerden Arrancar en la ruta ("http://127.0.0.1:4000/") o ("http:://localhost:4000/")
-app.run(debug=True, port=4000, host="0.0.0.0")
+# app.run(debug=True, port=4000, host="0.0.0.0")
