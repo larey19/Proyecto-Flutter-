45ae1f168a4013d77818025847b7a74c346d879d
Noel <noelenriqueascanio@gmail.com>
2025-06-16 14:38:09 -0500

    Update config.py

--- config.py
+++ config.py
@@ -8,3 +8,5 @@ class config:
     MYSQL_USER      = os.getenv("MYSQL_USER")
     MYSQL_PASSWORD  = os.getenv("MYSQL_PASSWORD")
     MYSQL_DB        = os.getenv("MYSQL_DB")
+    MYSQL_PORT      = int(os.getenv('MYSQL_PORT', 3306))
+    JWT_SECRET_KEY  = os.getenv("JWT_SECRET_KEY")         
