12de8c6ef2b3c9e0d744c939342cb60423b4be20
Noel <noelenriqueascanio@gmail.com>
2025-06-16 14:38:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ mysql = MySQL(app)
 app.mysql=mysql
 routes(app)
 
-app.config['SECRET_KEY'] = "1234"
+#app.config['SECRET_KEY'] = "1234"
 
 # Recuerden Arrancar en la ruta ("http://127.0.0.1:4000/") o ("http:://localhost:4000/")
 # app.run(debug=True, port=4000, host="0.0.0.0")
