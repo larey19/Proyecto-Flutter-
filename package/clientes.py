d4ecc41141acb07373fc7081c82858a10e0c2310
Noel <noelenriqueascanio@gmail.com>
2025-07-23 18:06:39 -0500

    Update clientes.py

--- package/clientes.py
+++ package/clientes.py
@@ -12,8 +12,8 @@ def GETclientes():
     CLIENTES = []
     for cliente in sql: #Recorrer todos los resutados de la consulta
         CLIENTES.append({ #Creamos tipo Diccinario CLAVE : VALOR
-            "cli_usu_id":cliente[0], #La clave tal cual como la tenemos en la base de datos "cli_usu_id" y el valor seran los indices [Posicion] de recorrer la consulta for ("CLIENTE") in sql
-            "usu_nombre":cliente[1] ,
+            "cli_id":cliente[11], #La clave tal cual como la tenemos en la base de datos "cli_usu_id" y el valor seran los indices [Posicion] de recorrer la consulta for ("CLIENTE") in sql
+            "usu_nombre":cliente[1] , 
             "usu_apellido":cliente[2],
             "usu_telefono":cliente[3], 
             "usu_correo":cliente[4], 
@@ -33,21 +33,20 @@ def POSTcliente():
     data = request.get_json(silent=True)  
     if data is None:
         return jsonify({"error": "Error en la formacion del JSON"}), 400
-    if 'cli_usu_id' in request.json:
-        cli_usu_id = request.json["cli_usu_id"]
+    if 'usu_num_doc' in request.json:
+        usu_num_doc = request.json["usu_num_doc"]
         cursor = current_app.mysql.connection.cursor()
-        cursor.execute("SELECT usu_id FROM t_usuario WHERE usu_id = %s", (cli_usu_id,))
+        cursor.execute("SELECT usu_num_doc FROM t_usuario WHERE usu_num_doc = %s", (usu_num_doc,))
         sql = cursor.fetchone()
         if not sql: 
             return jsonify({"mensaje" : "No existe un usuario registrado con ese ID"}),404
-        cursor.execute("SELECT cli_usu_id FROM t_cliente WHERE cli_usu_id = %s", (cli_usu_id,))
+        cursor.execute("SELECT cli_id FROM t_usuario JOIN t_cliente ON usu_id = cli_usu_id WHERE usu_num_doc = %s", (usu_num_doc,))
         sql = cursor.fetchone()
         if sql: 
             return jsonify({"mensaje" : "Ya existe un Cliente registrado con ese ID"}),409
         
         cursor = current_app.mysql.connection.cursor() #hacemos la conexion
-        cursor.execute("INSERT INTO t_cliente (cli_usu_id) SELECT usu_id FROM t_usuario LEFT JOIN t_cliente ON cli_usu_id = usu_id WHERE usu_id = %s", (cli_usu_id,)) #realiazamos consulta sql
-        cursor = current_app.mysql.connection.cursor() #hacemos conexion
+        cursor.execute("INSERT INTO t_cliente (cli_usu_id) SELECT usu_id FROM t_usuario LEFT JOIN t_cliente ON cli_usu_id = usu_id WHERE usu_num_doc = %s", (usu_num_doc,)) #realiazamos consulta sql
         cursor.connection.commit()
         return jsonify({"mensaje":"Se ha registrado el cliente"}),200
     else:
