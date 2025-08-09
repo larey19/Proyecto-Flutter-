c9d63eac8b6c15bd9595679ca73ac96c7a5dfcda
Noel <noelenriqueascanio@gmail.com>
2025-08-09 18:06:33 -0500

    Update __init__.py

--- package/__init__.py
+++ package/__init__.py
@@ -4,6 +4,7 @@ from .barberos import barberos_bp
 from .servicios import servicios_bp
 from .detalle_servicio import detal_servicios_bp
 from .documentacion import documentacion_bp
+from .reservas import reservas_bp
 from .login import login_bp
 
 def routes(app):
@@ -12,6 +13,7 @@ def routes(app):
     app.register_blueprint(clientes_bp)
     app.register_blueprint(barberos_bp)
     app.register_blueprint(servicios_bp)
+    app.register_blueprint(reservas_bp)
     app.register_blueprint(detal_servicios_bp)
     app.register_blueprint(login_bp)
     app.register_blueprint(documentacion_bp)
