8874279edf024159956cb84864e7a835e6bdad7e
Noel <noelenriqueascanio@gmail.com>
2025-09-25 22:27:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask
-from flask_mysqldb import MySQL  
+from flask_mysqldb import MySQL 
 from config import config
 from package import routes
+import os
 
 app = Flask(__name__)
 app.config.from_object(config)
@@ -9,7 +10,5 @@ mysql = MySQL(app)
 app.mysql=mysql
 routes(app)
 
-app.config['SECRET_KEY'] = "1234"
+app.config['JWT_KEY'] = os.getenv("JWT_KEY")
 
-# Recuerden Arrancar en la ruta ("http://127.0.0.1:4000/") o ("http:://localhost:4000/")
-# app.run(debug=True, port=4000, host="0.0.0.0")
