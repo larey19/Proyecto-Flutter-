9aab438d4a25a99e4c5dac7f748b7e984de8e4c5
Noel <noelenriqueascanio@gmail.com>
2025-09-25 23:20:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_mysqldb import MySQL 
 from config import config
 from package import routes
-import os
 
 app = Flask(__name__)
 app.config.from_object(config)
@@ -10,5 +9,4 @@ mysql = MySQL(app)
 app.mysql=mysql
 routes(app)
 
-app.config['JWT_KEY'] = os.getenv("JWT_KEY")
 
