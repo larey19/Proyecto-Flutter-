e12b7cf855575694a3c95c61eb24f5434cfd0b44
Noel <noelenriqueascanio@gmail.com>
2025-09-25 23:18:43 -0500

    Update auth.py

--- package/auth.py
+++ package/auth.py
@@ -1,26 +1,25 @@
+from flask import request, jsonify
+from dotenv import load_dotenv
 from functools import wraps
-from flask import request, jsonify, current_app
 import jwt
+import os 
+load_dotenv()
 
+JWT_KEY = os.getenv("JWT_KEY")
 def token(f):
     @wraps(f)
     def decorated(*args, **kwargs):
-        token = None
-        # if 'Authorization' in request.headers:
-        #     token = request.headers['Authorization']
-        
+        token = None        
         token = request.headers.get('Authorization')
         if token and token.startswith('Bearer '):
             token = token.split(" ")[1]
         if not token:
             return jsonify({"mensaje": "Token requerido"})
         try:
-            data = jwt.decode(token, current_app.config['SECRET_KEY'], algorithms=["HS256"])
+            data = jwt.decode(token, JWT_KEY, algorithms=["HS256"])
             request.usuario_id = data['usuario_id']
             request.nombre = data.get('nombre')
 
-        except jwt.ExpiredSignatureError:
-            return jsonify({"mensaje": "Token expirado"})
         except jwt.InvalidTokenError:
             return jsonify({"mensaje": "Token inválido"})
         return f(*args, **kwargs)
